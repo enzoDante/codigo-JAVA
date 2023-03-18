671585cdd92f15f0f44a5925f04d8850747ac7ed
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2023-03-18 14:19:30 -0300

    Create Projeto1Bpt2.java

---
+++ Projeto1Bpt2.java
@@ -0,0 +1,37 @@
+public class Projeto1Bpt2{
+
+    public static void main(String args[]){
+        int indiceG = 1, indiceTotal = 2, DescubraX = 0;
+        double x = 0;
+
+        //dias que formou os grupos
+        for(int i = 0; i < Integer.parseInt(args[0]); i++){
+            //caso total de alunos seja par:
+            if(Integer.parseInt(args[indiceTotal]) % 2 == 0){
+                //descobrir x
+                for(int z = 1; z < Integer.parseInt(args[indiceG]); z++){
+                    DescubraX += 2; // 0, 2, 4, 6...
+                    x += DescubraX; // 0 + 2 + 4 + 6 + ...
+                }
+                //calculando x: (total pessoas - x) / total de grupos
+                x = (Double.parseDouble(args[indiceTotal]) - x) / Integer.parseInt(args[indiceG]);
+                
+    
+                //inserindo na equação do grupo, porem temos x
+                for(int z = 1; z <= Integer.parseInt(args[indiceG]); z++){
+                    System.out.printf("%1.0f, ", x);
+                    x += 2;
+                }
+            }else
+                System.out.println("o total de alunos deve ser par!");
+
+            System.out.println("");
+
+            indiceG += 2;
+            indiceTotal += 2;
+            DescubraX = 0;
+            x = 0;
+        }
+
+    }
+}
