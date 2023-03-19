2143ef5cb79b63696c4a6bee3d381f361d464874
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2023-03-19 17:02:03 -0300

    Update Projeto1B.java

--- Projeto1B.java
+++ Projeto1B.java
@@ -11,19 +11,19 @@
  * ******************************************************************/
 public class Projeto1B{
         public static void main(String args[]){
-               System.out.println("teste");
                int total = args.length / 3;
                double totalMenor21 = 0;
                boolean maiorde60 = false;
                double totalMulheres = 0, totalHomens = 0;
-               int totalInterior = 0;
+               double totalInterior = 0;
+               double mediaMulher = 0, mediaHomens = 0;
                double porcHomens = 0, porcMulheres = 0;
-               
+                              
                //idade
                int i1 = 1, i2 = 2;
                for(int i=0; i <= args.length-1; i+=3){
                        int idade = 2023 - Integer.parseInt(args[i]);
-                       System.out.printf("tes %s e idade %d \n", args[i], idade);
+                       //System.out.printf("tes %s e idade %d \n", args[i], idade);
                        //idade
                        if(idade < 21)
                                 totalMenor21++;
@@ -45,32 +45,23 @@ public class Projeto1B{
                         i1 += 3;
                         i2 += 3;
                }
-               System.out.printf("%1.0f \n", totalMenor21);
-               System.out.printf("total %d \n", total); 
+               //System.out.printf("%1.0f \n", totalMenor21);
+               //System.out.printf("total %d \n", total); 
                double porcMenor21 = (totalMenor21 / total) * 100;
+
+               mediaMulher = total / totalMulheres;
+               mediaHomens = total / totalHomens;
+
                porcMulheres = (totalMulheres / total) * 100;
                porcHomens = (totalHomens / total) * 100;
                
                System.out.printf("Porcentagem de motoristas menor de 21 anos: %1.2f%% \n", porcMenor21);  
-               System.out.printf("Total de mulheres: %1.0f \n", totalMulheres);
-               System.out.printf("Total de pessoas do interior: %d \n", totalInterior);
+               System.out.printf("Total de mulheres: %1.2f \n", totalMulheres);
+               System.out.printf("Total de pessoas do interior: %1.2f \n", totalInterior);
                System.out.printf(maiorde60 ? "Existe alguem acima de 60 anos \n" : "Nao existe alguem acima de 60 anos \n");
+               System.out.printf("Media de mulheres: %1.2f \n", mediaMulher);
+               System.out.printf("Media de homens: %1.2f \n", mediaHomens);
                System.out.printf("%1.2f%% sao homens e %1.2f%% sao mulheres \n", porcHomens, porcMulheres);
-               
-               //genero
-               /*
-                for(int i=1; i<= args.length-1; i += 3)
-                        if(Integer.parseInt(args[i]) == 2) totalMulheres++;
-
-                System.out.printf("Total de mulheres: %d \n", totalMulheres);
-
-                //procedencia
-                for(int i=2; i<= args.length-1; i += 3)
-                        if(Integer.parseInt(args[i]) == 1) totalInterior++;
-
-                System.out.printf("Total de pessoas do interior: %d \n", totalInterior); */
-                
-                               
-
+              
         }
 }
