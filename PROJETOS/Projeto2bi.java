83ffcc28749d39ac4820bc92aad1fc2cbf81a637
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2023-05-16 21:32:17 -0300

    começo de projeto 2 bimestre
    feito: inserir candidata
    falta:
    organizar em colocações;
    sistema de opções;
    consultar;
    alterar;
    excluir;
    relatar;
    sair.

---
+++ PROJETOS/Projeto2bi.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Projeto2bi{
+        public static void main(String args[]){
+                Scanner scan = new Scanner(System.in);
+                //int contCandidatas = 0;
+                //ClasseCandidatas[] candidatas = new ClasseCandidatas[1];
+
+                ClasseOpcoes escolhas = new ClasseOpcoes();
+                while(true){
+                        escolhas.InserirCandidata();
+                        /*System.out.println("Digite o nome da candidata:");
+                        String nome = scan.nextLine();
+                        //ClasseCandidatas candidata = new ClasseCandidatas(nome);
+
+                        if(contCandidatas > 0){
+                                //array com espaço a mais(nova candidata)
+                                ClasseCandidatas[] novasCandidatas = new ClasseCandidatas[candidatas.length+1];
+                                //salvando candidatas anteriores
+                                for(int i = 0; i < candidatas.length; i++){
+                                        novasCandidatas[i] = new ClasseCandidatas(candidatas[i].getNome());
+                                }
+                                //criando a nova candidata
+                                novasCandidatas[candidatas.length] = new ClasseCandidatas(nome);
+
+                                //array original que vai ter todas as candidatas
+                                candidatas = new ClasseCandidatas[novasCandidatas.length];
+                                for(int i = 0; i < contCandidatas+1; i++){
+                                        candidatas[i] = new ClasseCandidatas(novasCandidatas[i].getNome());
+                                }
+                        }else
+                                //caso seja o primeiro registro
+                                candidatas[contCandidatas] = new ClasseCandidatas(nome);
+
+                        System.out.println("Deseja continuar? 1-sim/2-nao");
+                        int escolha = Integer.parseInt(scan.nextLine());
+                        if(escolha != 1)
+                                break;
+                        contCandidatas++;*/
+                        System.out.println("Deseja continuar? 1-sim/2-nao");
+                        int escolha = Integer.parseInt(scan.nextLine());
+                        if(escolha != 1)
+                                break;
+
+                }
+                //contCandidatas
+                for(int i = 0; i < escolhas.getContCandidatas(); i++){
+                        System.out.println(escolhas.getCandidataNome(i));//candidatas[i].getNome()
+                }
+
+        }
+}
