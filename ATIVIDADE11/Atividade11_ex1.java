15dfd4d5bd3a1898a8ee756394f42cf0d634a402
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2023-09-26 17:08:26 -0300

    ex 1 e 2

--- ATIVIDADE11/Atividade11_ex1.java
+++ ATIVIDADE11/Atividade11_ex1.java
@@ -3,17 +3,51 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Atividade11_ex1 extends JFrame{
+    private String texto = "";
+    JButton btn;
+    JTextField txt;
+    JLabel rotulo;
+    List lista;
 
-public Atividade11_ex1(){
-       Container JANELA = getContentPane();
-       JANELA.setLayout(null);
+    public Atividade11_ex1(){
+        Container tela = getContentPane();
+        tela.setLayout(null);
+        rotulo = new JLabel("Digite uma frase:");
+        tela.add(rotulo);
+        rotulo.setBounds(10,5,200,20);
 
-       setSize(400,300);
-       setVisible(true);
+        txt = new JTextField();
+        tela.add(txt);
+        txt.setBounds(10, 30, 200, 20);
 
-}
-        public static void main(String args[]){
-            Atividade11_ex1 app = new Atividade11_ex1();
-            app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        }
+        lista = new List(4, false);
+        tela.add(lista);
+        lista.setBounds(20, 60, 150, 50);
+
+        btn = new JButton("Ok");
+        tela.add(btn);
+        btn.setBounds(50, 150, 50, 30);
+
+        btn.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evento){
+                //JOptionPane.showMessageDialog(null, txt.getText());
+                setTexto(txt.getText());
+                lista.addItem(getTexto());
+            }
+        });
+
+        setSize(400,300);
+        setVisible(true);
+
+    }
+    public void setTexto(String value){
+        this.texto = value;
+    }
+    public String getTexto(){
+        return this.texto;
+    }
+    public static void main(String args[]){
+        Atividade11_ex1 app = new Atividade11_ex1();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
---
+++ ATIVIDADE11/Atividade11_ex2.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class Atividade11_ex2 extends JFrame{
+    private String texto = "";
+    JButton btn;
+    List lista;
+
+    public Atividade11_ex2(){
+        Container tela = getContentPane();
+        tela.setLayout(null);
+
+        lista = new List();
+        tela.add(lista);
+        lista.setBounds(20, 20, 150, 150);
+
+        btn = new JButton("Ok");
+        tela.add(btn);
+        btn.setBounds(50, 200, 50, 30);
+
+        btn.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evento){
+                //JOptionPane.showMessageDialog(null, txt.getText());
+                gerarNumeros(10, lista);
+            }
+        });
+
+        setSize(400,300);
+        setVisible(true);
+
+    }
+    public void gerarNumeros(int quant, List lis){
+        Random gerador = new Random();
+        for(int i = 0; i < quant; i++){
+            lis.addItem((gerador.nextInt(61)+30)+"");
+        }
+    }
+    public static void main(String args[]){
+        Atividade11_ex2 app = new Atividade11_ex2();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
